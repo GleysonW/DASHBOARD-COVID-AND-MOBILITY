11b063a1aff8598e05b81963c375dd6775068047
GleysonW <gleysonwenderson3@gmail.com>
2024-10-29 22:09:47 -0300

    update

--- dashboard.py
+++ dashboard.py
@@ -6,7 +6,8 @@
 #-=- Bibliotecas usadas -=-
 import os
 import dash
-from dash import dcc, html
+import dash_core_components as dcc
+import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
